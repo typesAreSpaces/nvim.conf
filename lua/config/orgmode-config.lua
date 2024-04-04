62022369e5d83c9525543a70d716ef87bb7b2789
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-04-04 13:16:07 -0600

    Minor changes.

--- lua/config/orgmode-config.lua
+++ lua/config/orgmode-config.lua
@@ -1,5 +1,3 @@
-require('orgmode').setup_ts_grammar()
-
 -- Tree-sitter configuration
 require'nvim-treesitter.configs'.setup {
   -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
